73d0a65845491bab6b046737385a3edc5ffb8128
Maureen Byrne <maureen.byrne@dnb.no>
2021-05-25 21:50:40 +0200

    correct failing test

--- click-ex/test_hello.py
+++ click-ex/test_hello.py
@@ -2,7 +2,7 @@ from click import testing
 from hello import hello
 from click.testing import CliRunner
 
-def if_present_in_input_then__prensent_in_output():
+def test_hello():
     runner = CliRunner()
     result = runner.invoke(hello, ["--name", "Thor", "--color", "blue"])
     assert "Thor" in result.output
